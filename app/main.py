ac2f61533cc9c7f876a0a2827b2b36d8e45d0b07
m1xxos <66390094+m1xxos@users.noreply.github.com>
2022-12-10 00:23:45 +0300

    basic user authentication and dockerfile

---
+++ app/main.py
@@ -0,0 +1,56 @@
+from fastapi import Depends, FastAPI
+
+from app.users.db import User, create_db_and_tables
+from app.users.schemas import UserCreate, UserRead, UserUpdate
+from app.users.users import auth_backend, current_active_user, fastapi_users
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+
+
+@app.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
+
+
+@app.on_event("startup")
+async def on_startup():
+    # Not needed if you setup a migration system like Alembic
+    await create_db_and_tables()
